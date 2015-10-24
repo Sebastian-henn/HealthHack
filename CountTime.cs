c7149314522b3aface0a8b2bb52d8e9271b14b10
Seb <sebastian.henn@gmail.com>
2015-10-24 16:58:25 +0800

    Counter and MeshColour

---
+++ CountTime.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+public class CountTime : MonoBehaviour 
+{
+	private GazeAwareComponent _gazeAware;
+
+	public FaceObject Happy = new FaceObject("Happy");
+	public FaceObject Sad = new FaceObject("Sad");
+	public FaceObject Confused = new FaceObject("Confused");
+	private FaceObject currentFace = new FaceObject("Current");
+	private Stopwatch attentionTimer = new Stopwatch();
+	private Stopwatch distractionTimer = new Stopwatch();
+
+
+	public int counter = 0;
+
+	void Start () 
+	{
+		_gazeAware = GetComponent<GazeAwareComponent>();
+	}
+	
+	void Update () 
+	{
+
+		if (_gazeAware.HasGaze) {
+			attentionTimer.Start ();
+			distractionTimer.Stop ();
+			distractionTimer.Reset();
+		
+			switch (this.tag) {
+			case "HappyTag":
+				currentFace = Happy;
+				break;
+			case "SadTag":
+				currentFace = Sad;
+				break;
+			case "ConfusedTag":
+				currentFace = Confused;
+				break;
+			}
+
+			thisFace().counter = attentionTimer.Elapsed.TotalMilliseconds;
+			print ("Face = " + thisFace().name + " Counter =" + thisFace().counter);
+		} else {
+			attentionTimer.Stop();
+			distractionTimer.Start();
+
+			if(distractionTimer.ElapsedMilliseconds > 500) {
+				attentionTimer.Reset();
+			}
+		}
+	}
+	
+	public FaceObject thisFace() 
+	{
+		switch (this.tag) {
+		case "HappyTag":
+			return Happy;
+		case "SadTag":
+			return Sad;;
+		case "ConfusedTag":
+			return Confused;
+		default:
+			return null;
+		}
+	}
+}
---
+++ MeshColour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshColour : MonoBehaviour
+{
+	public Color colourStart = Color.white;
+	public Color colourEnd = Color.green;
+	public float duration = 1.0F;
+
+	private Color currentColor;
+	private Material materialColored;
+	public Renderer rend;
+
+	void Start()
+	{
+		rend = GetComponent<Renderer>();
+		//rend.material.shader = Shader.Find ("Specular");
+	}
+
+	void Update()
+	{
+		FaceObject currentFace = this.GetComponent<CountTime> ().thisFace ();
+		float value = (float)((currentFace.counter / 5000));
+		if (value > duration)
+			value = duration;
+		//print (value);
+
+		float lerp = Mathf.PingPong (value, duration) / duration;
+
+		//Color col = new Color (10, 10, value);
+		rend.material.color = Color.Lerp (colourStart,colourEnd,lerp);
+	}
+
+
+}
